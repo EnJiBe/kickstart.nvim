b9ca8bbbdb51564c7e5a7d65536864d11ea777c4
EnJiBe <nicolas.junod1@heig-vd.ch>
2026-05-28 15:56:09 +0200

    Forgot a conflict.

--- init.lua
+++ init.lua
@@ -245,9 +245,9 @@ do
 
   -- If you prefer more traditional completion keymaps,
   -- you can uncomment the following lines
-  ['<CR>'] = cmp.mapping.confirm { select = true },
-  ['<Tab>'] = cmp.mapping.select_next_item(),
-  ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  -- ['<CR>'] = cmp.mapping.confirm { select = true },
+  -- ['<Tab>'] = cmp.mapping.select_next_item(),
+  -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
   -- Highlight when yanking (copying) text
   --  Try it with `yap` in normal mode
@@ -982,38 +982,8 @@ do
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-<<<<<<< HEAD
-  { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
-=======
   -- require 'custom.plugins'
 end
->>>>>>> master
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
